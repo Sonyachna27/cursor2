6e5ef83c5d9fcecb4702d452939d1d8d555f7bad
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2023-03-08 00:43:42 +0200

    change solution

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,23 +1,32 @@
-let question = +prompt('Write first number', '0');
-let question2 = +prompt('Write second number', '0');
-          
-    let num = Number.isInteger(question);
-    let num1 = Number.isInteger(question2);
-          if((num === true) && (num1 === true)){ 
-          if (question > question2){
-            alert ('The number first must be smaller');
-            } else {
-              let sum = 0;
-              if (confirm('Do you need to skip even numbers?(yes/ok or no/cancel)') === true){
-                for (let i = question; i <= question2; i++ ){
-                  if (i % 2 === 0)
-                    sum+=i;
-                }
-             } else {
-              for (let i = question; i <= question2; i++ ){
-                sum+=i;
-              }
-             }
-            alert (`Sum of numbers = ${sum}`);
-             }
-            }
+            
+            
+let numN ;
+let numM ;
+let sum = 0;
+let integerN= Number.isInteger(numN);
+let integerM = Number.isInteger(numM)
+do {
+    numN = +prompt("Enter the first Number");
+}
+while (!integerN && numN < 0);
+
+do {
+    numM = +prompt("Enter the second Number");
+}
+while (!integerM && numN >= numM);
+
+const skipNumber = confirm("Do you need to skip even numbers?(yes/ok or no/cancel)");
+                
+                  for (let i = numN; i <= numM; i++ ){
+                    if (skipNumber === true){
+                      sum +=i;
+                    }
+                    else{
+                      if (i % 2 === 0) continue
+                      sum +=i;
+                  }
+                    }
+
+               alert (`Sum of numbers = ${sum}`);
+              
+ 
