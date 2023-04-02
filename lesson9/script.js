169c4b442b7742d0e498ee674fc73dad7e413d2a
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2023-04-02 21:45:08 +0300

    add hw DOM

---
+++ lesson9/script.js
@@ -0,0 +1,31 @@
+document.body.onload = generateBlocks();
+
+
+ function generateBlocks() {
+  const wrapper = document.querySelector('#wrap');
+    wrap.style.cssText =`width: 250px;
+    height: 250px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;`;
+    wrapper.innerHTML = '';
+          for (let j = 0; j < 25; j++) {
+            let block = document.createElement('div');
+            block.classList.add('block');
+           wrapper.append(block);
+          }
+ }
+ 
+  function generateBlocksInterval(){
+              const blocks = document.getElementsByClassName('block');
+                  function getRandom(min, max){
+                  return Math.ceil(Math.random() * (max - min) + min)
+                  }
+                 for( let i =0; i < blocks.length; i++){
+                  blocks[i].style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
+                 }
+              }
+
+ setInterval(generateBlocksInterval, 1000);
+
+ 
