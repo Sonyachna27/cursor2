2ea3d0dbd32c32d92a4aedc55c7269bfef1fc76f
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2023-04-08 14:18:09 +0300

    add hw11

---
+++ lesson11/script.js
@@ -0,0 +1,22 @@
+async function getRandomChinese (length) {
+    let sign = (Date.now() + '').split('').slice(-5).join('');
+    const res = String.fromCharCode(sign);
+    let result = '';
+    let i = length;
+    while(i > 1){
+        const res = String.fromCharCode(sign);
+        result += res;   
+          i--;
+} 
+return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(result += res);
+    }, i*500);
+  }).then(console.log)
+   
+} 
+
+console.log(getRandomChinese(5))
+
+    
+   
